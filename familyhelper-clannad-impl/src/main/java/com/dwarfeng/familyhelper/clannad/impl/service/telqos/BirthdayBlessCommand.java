bf84e0b8e0e14e93a517cd97144379dbd72a4b62
DwArFeng <915724865@qq.com>
2024-01-28 18:44:34 +0800

    升级 spring-telqos 并应用其新功能
    使用包扫描的方式注册指令
    优化 `telqos/connection.properties` 中配置的键名
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- familyhelper-clannad-impl/src/main/java/com/dwarfeng/familyhelper/clannad/impl/service/telqos/BirthdayBlessCommand.java
+++ familyhelper-clannad-impl/src/main/java/com/dwarfeng/familyhelper/clannad/impl/service/telqos/BirthdayBlessCommand.java
@@ -1,18 +1,18 @@
 package com.dwarfeng.familyhelper.clannad.impl.service.telqos;
 
 import com.dwarfeng.familyhelper.clannad.stack.service.BirthdayBlessQosService;
+import com.dwarfeng.springtelqos.node.config.TelqosCommand;
 import com.dwarfeng.springtelqos.sdk.command.CliCommand;
 import com.dwarfeng.springtelqos.stack.command.Context;
 import com.dwarfeng.springtelqos.stack.exception.TelqosException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
 import org.apache.commons.lang3.tuple.Pair;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@TelqosCommand
 public class BirthdayBlessCommand extends CliCommand {
 
     private static final String COMMAND_OPTION_ONLINE = "online";
