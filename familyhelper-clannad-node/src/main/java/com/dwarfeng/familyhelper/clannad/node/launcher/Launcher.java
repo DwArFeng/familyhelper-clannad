7be741d6599cb76842f9a1aa3b6e494a2c3e0d1a
DwArFeng <915724865@qq.com>
2024-04-18 22:25:42 +0800

    启动器优化
    将入口方法中完整独立的功能封装在子方法中，使入口方法代码结构更加清晰
    
    Signed-off-by: DwArFeng <915724865@qq.com>

--- familyhelper-clannad-node/src/main/java/com/dwarfeng/familyhelper/clannad/node/launcher/Launcher.java
+++ familyhelper-clannad-node/src/main/java/com/dwarfeng/familyhelper/clannad/node/launcher/Launcher.java
@@ -6,6 +6,7 @@ import com.dwarfeng.springterminator.sdk.util.ApplicationUtil;
 import com.dwarfeng.subgrade.stack.exception.ServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.Date;
@@ -26,59 +27,80 @@ public class Launcher {
                 "file:opt/opt*.xml",
                 "file:optext/opt*.xml"
         }, ctx -> {
-            LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
-
-            // 拿出程序中的 ThreadPoolTaskScheduler，用于处理计划任务。
-            ThreadPoolTaskScheduler scheduler = ctx.getBean(ThreadPoolTaskScheduler.class);
-
-            // 处理生日祝福处理器的启动选项。
-            BirthdayBlessQosService birthdayBlessQosService = ctx.getBean(BirthdayBlessQosService.class);
-            // 生日祝福处理器是否上线生日祝福服务。
-            long onlineDriveDelay = launcherSettingHandler.getOnlineBirthdayBlessDelay();
-            if (onlineDriveDelay == 0) {
-                LOGGER.info("立即上线生日祝福服务...");
-                try {
-                    birthdayBlessQosService.online();
-                } catch (ServiceException e) {
-                    LOGGER.error("无法上线生日祝福服务，异常原因如下", e);
-                }
-            } else if (onlineDriveDelay > 0) {
-                LOGGER.info(onlineDriveDelay + " 毫秒后上线生日祝福服务...");
-                scheduler.schedule(
-                        () -> {
-                            LOGGER.info("上线生日祝福服务...");
-                            try {
-                                birthdayBlessQosService.online();
-                            } catch (ServiceException e) {
-                                LOGGER.error("无法上线生日祝福服务，异常原因如下", e);
-                            }
-                        },
-                        new Date(System.currentTimeMillis() + onlineDriveDelay)
-                );
-            }
-            // 生日祝福处理器是否启动生日祝福服务。
-            long startBirthdayBlessDelay = launcherSettingHandler.getStartBirthdayBlessDelay();
-            if (startBirthdayBlessDelay == 0) {
-                LOGGER.info("立即启动生日祝福服务...");
-                try {
-                    birthdayBlessQosService.online();
-                } catch (ServiceException e) {
-                    LOGGER.error("无法启动生日祝福服务，异常原因如下", e);
-                }
-            } else if (startBirthdayBlessDelay > 0) {
-                LOGGER.info(startBirthdayBlessDelay + " 毫秒后启动生日祝福服务...");
-                scheduler.schedule(
-                        () -> {
-                            LOGGER.info("启动生日祝福服务...");
-                            try {
-                                birthdayBlessQosService.start();
-                            } catch (ServiceException e) {
-                                LOGGER.error("无法启动生日祝福服务，异常原因如下", e);
-                            }
-                        },
-                        new Date(System.currentTimeMillis() + startBirthdayBlessDelay)
-                );
-            }
+            // 根据启动器设置处理器的设置，选择性上线生日祝福服务。
+            mayOnlineBirthdayBless(ctx);
+            // 根据启动器设置处理器的设置，选择性启动生日祝福服务。
+            mayStartBirthdayBless(ctx);
         });
+    }
+
+    private static void mayOnlineBirthdayBless(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 获取程序中的 ThreadPoolTaskScheduler，用于处理计划任务。
+        ThreadPoolTaskScheduler scheduler = ctx.getBean(ThreadPoolTaskScheduler.class);
+
+        // 获取生日祝福 QOS 服务。
+        BirthdayBlessQosService birthdayBlessQosService = ctx.getBean(BirthdayBlessQosService.class);
+
+        // 判断生日祝福处理器是否上线生日祝福服务，并按条件执行不同的操作。
+        long onlineBirthdayBlessDelay = launcherSettingHandler.getOnlineBirthdayBlessDelay();
+        if (onlineBirthdayBlessDelay == 0) {
+            LOGGER.info("立即上线生日祝福服务...");
+            try {
+                birthdayBlessQosService.online();
+            } catch (ServiceException e) {
+                LOGGER.error("无法上线生日祝福服务，异常原因如下", e);
+            }
+        } else if (onlineBirthdayBlessDelay > 0) {
+            LOGGER.info("{} 毫秒后上线生日祝福服务...", onlineBirthdayBlessDelay);
+            scheduler.schedule(
+                    () -> {
+                        LOGGER.info("上线生日祝福服务...");
+                        try {
+                            birthdayBlessQosService.online();
+                        } catch (ServiceException e) {
+                            LOGGER.error("无法上线生日祝福服务，异常原因如下", e);
+                        }
+                    },
+                    new Date(System.currentTimeMillis() + onlineBirthdayBlessDelay)
+            );
+        }
+    }
+
+    private static void mayStartBirthdayBless(ApplicationContext ctx) {
+        // 获取启动器设置处理器，用于获取启动器设置，并按照设置选择性执行功能。
+        LauncherSettingHandler launcherSettingHandler = ctx.getBean(LauncherSettingHandler.class);
+
+        // 获取程序中的 ThreadPoolTaskScheduler，用于处理计划任务。
+        ThreadPoolTaskScheduler scheduler = ctx.getBean(ThreadPoolTaskScheduler.class);
+
+        // 获取生日祝福 QOS 服务。
+        BirthdayBlessQosService birthdayBlessQosService = ctx.getBean(BirthdayBlessQosService.class);
+
+        // 判断生日祝福处理器是否启动生日祝福服务，并按条件执行不同的操作。
+        long startBirthdayBlessDelay = launcherSettingHandler.getStartBirthdayBlessDelay();
+        if (startBirthdayBlessDelay == 0) {
+            LOGGER.info("立即启动生日祝福服务...");
+            try {
+                birthdayBlessQosService.start();
+            } catch (ServiceException e) {
+                LOGGER.error("无法启动生日祝福服务，异常原因如下", e);
+            }
+        } else if (startBirthdayBlessDelay > 0) {
+            LOGGER.info("{} 毫秒后启动生日祝福服务...", startBirthdayBlessDelay);
+            scheduler.schedule(
+                    () -> {
+                        LOGGER.info("启动生日祝福服务...");
+                        try {
+                            birthdayBlessQosService.start();
+                        } catch (ServiceException e) {
+                            LOGGER.error("无法启动生日祝福服务，异常原因如下", e);
+                        }
+                    },
+                    new Date(System.currentTimeMillis() + startBirthdayBlessDelay)
+            );
+        }
     }
 }
